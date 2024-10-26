46c77453a74ed35e1fd4acf942803b8a3532a30b
Felipe-Tommaselli <f.tommaselli@usp.br>
2024-10-25 23:38:04 -0300

    LendingContract done

--- contracts/LendingContract.sol
+++ contracts/LendingContract.sol
@@ -1,20 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract LoanContract {
+contract LendingPlatform {
     address public owner;
-    mapping(address => uint256) public balances;
 
+    // Mapping to track user loan balances
+    mapping(address => uint256) public loanBalances;
+
+    // Event to log loan requests
     event LoanRequested(address indexed borrower, uint256 amount, bool approved);
 
+    // Constructor to set the contract owner
     constructor() {
         owner = msg.sender;
     }
 
+    // Function to request a loan
     function requestLoan(uint256 amount, bool approved) public {
         if (approved) {
-            balances[msg.sender] += amount;
+            loanBalances[msg.sender] += amount;
         }
+
+        // Emit the loan request event
         emit LoanRequested(msg.sender, amount, approved);
+    }
+
+    // Function to check the loan balance of a user
+    function getLoanBalance() public view returns (uint256) {
+        return loanBalances[msg.sender];
     }
 }
